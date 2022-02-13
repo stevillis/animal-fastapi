8f10907daf4db9add95e015d3c32180424fef992
Stévillis Sousa <stevillis@hotmail.com>
2022-02-12 20:21:25 -0400

    Create Animal API

---
+++ api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from data import AnimalList
+
+animal_list = AnimalList()
+
+animal_router = APIRouter()
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from api import animal_router
+from data import AnimalList
+
+animal_list = AnimalList()
+app = FastAPI()
+
+
+@app.get('/', tags=['main'], response_class=HTMLResponse)
+def main():
+    """
+    View that shows description about the Animal API
+    :return: A link to the API instructions
+    """
+    return """<a href="http://localhost:8000/docs">API instructions</a>"""
+
+
+app.include_router(animal_router, prefix='/animal', tags=['animal'])
